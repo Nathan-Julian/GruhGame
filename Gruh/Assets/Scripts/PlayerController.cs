f043d2ab8d7d69ba811d5af0cbcbba7416c4e9ea
Nathan <nathanrjulian@gmail.com>
2022-08-11 17:12:04 -0700

    ok

--- Gruh/Assets/Scripts/PlayerController.cs
+++ Gruh/Assets/Scripts/PlayerController.cs
@@ -11,8 +11,12 @@ public class PlayerController : MonoBehaviour
     public float groundDrag;
     public float airDrag;
     public float groundAccel;
+    public float sprintAccel;
     public float airAccel;
     public float groundSpeedCap;
+    public float jumpForce;
+    public float mouseSensitivity;
+    
 
     float GroundCastLength = 2;
 
@@ -21,13 +25,14 @@ public class PlayerController : MonoBehaviour
     string rightKey = "d";
     string forwardKey = "w";
     string backKey = "s";
+    string jumpKey = "space";
+    string crouchKey = "shift";
 
-
-
+    float xRotation = 0f;
 
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
 
@@ -44,6 +49,11 @@ public class PlayerController : MonoBehaviour
         if (Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.TransformDirection(Vector3.down), out hit, GroundCastLength, layerMask))
         {
             //did hit - on ground
+            Debug.Log("touching Ground");
+            //ground actions
+
+
+
             Debug.DrawRay(PlayerCamera.transform.position, PlayerCamera.transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
             PlayerRB.drag = groundDrag;
             //input check
@@ -65,14 +75,30 @@ public class PlayerController : MonoBehaviour
             {
                 PlayerRB.AddRelativeForce(0, 0, -groundAccel * Time.deltaTime * 100);
             }
+            if(Input.GetKey(jumpKey))
+            {
+                PlayerRB.AddForce(0, jumpForce, 0);
+            }
+
+            
         }
         else
         {
             //didn't hit - in air
+            Debug.Log("in air");
             Debug.DrawRay(PlayerCamera.transform.position, PlayerCamera.transform.TransformDirection(Vector3.down) * 1000, Color.white);
             PlayerRB.drag = 0.01F;
             PlayerRB.AddForce(0, -gravity * Time.deltaTime, 0); //add gravity force
         }
+
+        //mouse input
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            transform.Rotate(Vector3.up * mouseX);
     }
 }
 
